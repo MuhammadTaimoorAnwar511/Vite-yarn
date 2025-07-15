15f78950126b2e965145051140b824efda71a0ce
Ubuntu <ubuntu@ip-172-31-20-128.ec2.internal>
2025-07-15 10:42:40 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from VITE App (yarn) 👋</h1>
+        <p>This is a simple message app created using yarn.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
